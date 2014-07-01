5b280ebc2429bae734523aecf6e8fbb7e2c5ee2b
Justin Oblak <jroblak@gmail.com>
2014-06-30 20:02:53 -0400

    ?

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'sagdc-game');
-var Player = require('./entities/Player');
+var Player = require('./entities/player');
 
 game.state.add('Boot', require('./states/boot'));
 game.state.add('Splash', require('./states/splash'));
